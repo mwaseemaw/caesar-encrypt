9398ec9bf9089a4d0baea57f2398f715eba5003a
Mohammed Waseem <81080516+mwaseemaw@users.noreply.github.com>
2023-05-25 13:33:26 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -15,16 +15,16 @@ def encrypt(text, shift):
 
 if __name__ == '__main__':
     print("=========================")
-    print("====CAESAR ENCRYPTION====")
-    print("=========================")
-    i = int(input('Enter \n1 to start \n0 to end \nNumber: '))
+    print("    CAESAR ENCRYPTION      A program that converts plain text using CAESARS Encryption")
+    print("=========================  https://github.com/mwaseemaw/caesar-encrypt.git")
+    i = int(input('Enter \n1 to start \n0 to end \nStart/End: '))
     while i == 1:
-        print("====PROGRAM STARTED====")
+        print("====PROGRAM STARTED====\n")
         txt = input("Enter text to encrypt: ")
         shft = int(input("Enter number to shift: "))
         res = encrypt(txt, shft)
         print("Encrypted text is: " + res)
         print("====THANK YOU FOR USING THIS PROGRAM====")
         i = int(input('Enter \n1 to start \n0 to end \nNumber: '))
-    print("====PROGRAM ENDED====")
+    print("\n====PROGRAM ENDED====")
 
